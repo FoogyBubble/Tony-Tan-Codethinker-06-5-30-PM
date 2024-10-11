07579214b3a18cbdea7c800219750159ba5979da
FoogyBubble <Tan_Ding_Sheng_Tony@students.edu.sg>
2024-10-11 10:45:27 +0000

    End of Lesson 14

---
+++ script/practice.js
@@ -0,0 +1,9 @@
+let my_home_address = "218 Lor 8 Toa Payoh #23-621 S310218"
+const pi = 3.142
+
+console.log(my_home_address)
+console.log(MY_NRIC)
+
+let tax = () => {
+    console.log("$80000")
+}
