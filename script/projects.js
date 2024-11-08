e6c3d9eba9744a9b4a71e444abe3ff6ea5a23b43
FoogyBubble <Tan_Ding_Sheng_Tony@students.edu.sg>
2024-11-08 10:43:47 +0000

    End of Lesson 18

--- script/projects.js
+++ script/projects.js
@@ -1,43 +1,68 @@
 let canvas, ctx;
-let CentreOfAttention1, CentreOfAttention2;
-let BallOfAttention1, BallOfAttention2;
-let CentreOfAttention3, CentreOfAttention4; //change value of BallOfAttention1 and BallOfAttention2
+let CentreX, CentreY;
+let BallX, BallY;
+let SpeedX, SpeedY //change value of BallX and BallY
+
+
+let DrawBall = () => {
+    ctx.lineWidth = 7;
+    ctx.strokeStyle = "rgb(219, 7, 9.2)";
+    ctx.fillStyle = "rgb(8, 9, 184)";
+
+    ctx.beginPath();
+    ctx.arc(BallX, BallY, 25, 0, 6.284);
+    ctx.stroke();
+    ctx.fill();
+}
+
+let animating = () => {
+   if (document.getElementById("playAnimation").checked) {
+        //erase everything
+        ctx.reset();
+
+        if (BallX < 0 || BallX > canvas.width) {
+           SpeedX = -SpeedX;
+        }
+        if (BallY < 0 || BallY > canvas.height) {
+           SpeedY = -SpeedY;
+        }
+
+        BallX = BallX + SpeedX;
+        BallY = BallY + SpeedY;
+        DrawBall();  
+   }
+
+   if (document.getElementById("playAnimationX2").checked) {
+         //erase everything
+         ctx.reset();
+
+         if (BallX < 0 || BallX > canvas.width) {
+            2(SpeedX) = -2(SpeedX);
+         }
+         if (BallY < 0 || BallY > canvas.height) {
+            2(SpeedY) = -2(SpeedY);
+         }
+      
+         BallX = BallX + 2(SpeedX);
+         BallY = BallY + 2(SpeedY);
+         DrawBall();  
+   }
+   requestAnimationFrame(animating); //call itself again
+}
 
 window.onload = () => {
     canvas = document.getElementById("drawingboard");
     ctx = canvas.getContext("2d");
     
-    CentreOfAttention1 = canvas.width / 2;
-    CentreOfAttention2 = canvas.height / 2;
+    CentreX = canvas.width / 2;
+    CentreY = canvas.height / 2;
 
-    BallOfAttention1 = CentreOfAttention1;
-    BallOfAttention2 = CentreOfAttention2;
+    BallX = CentreX;
+    BallY = CentreY;
 
-    CentreOfAttention3 = 7;
-    CentreOfAttention4 = 7;
+    SpeedX = 7;
+    SpeedY = 7;
 
     DrawBall();
-    RefreshAnimation();
-}
-
-let DrawBall = () => {
-    ctx.lineWidth = 0.7;
-    ctx.strokeStyle = "rgb(214, 70, 2)";
-    ctx.fillStyle = "rgb(7, 7, 219)"
-
-    ctx.beginPath();
-    ctx.arc(80, 80, 25, 0, 6.284)
-    ctx.stroke();
-    ctx.fill();
-}
-
-
-let RefreshAnimation = () => {
-    // erase everything
-    // ctx.reset();
-    BallOfAttention1 = BallOfAttention1 + CentreOfAttention3;
-    BallOfAttention2 = BallOfAttention2 + CentreOfAttention4;
-    DrawBall();
-
-    requestAnimationFrame(animating) // call itself again
+    animating();
 }
